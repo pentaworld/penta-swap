f79f741a935e908f45a6a48304980e79a7fa205b
inaridiy <inainainariinainari@gmail.com>
2022-07-19 16:35:28 +0900

    update CurrrencyInput

--- projects/interface/src/components/Currency/CurrencyInput.tsx
+++ projects/interface/src/components/Currency/CurrencyInput.tsx
@@ -7,12 +7,14 @@ import { CurrencySelectButton } from "./CurrencySelectButton";
 
 // eslint-disable-next-line react/display-name
 export const CurrencyInput: React.FC<{
+  label: string;
   currency: Currency | Token | null;
   onSelect: (currency: Currency | Token | null) => void;
-  label: string;
+  value: string;
+  onChange: () => string;
 }> =
   // eslint-disable-next-line react/display-name
-  memo(({ currency, onSelect, label }) => {
+  memo(({ currency, onSelect, value, onChange, label }) => {
     const { isOpen, open, close } = useModal(`select-${label}`);
     return (
       <>
@@ -29,7 +31,11 @@ export const CurrencyInput: React.FC<{
           <div className="flex flex-col gap-1 justify-between sm:flex-row sm:items-center">
             <CurrencySelectButton currency={currency} onClick={open} />
             <div className="-m-1 divider sm:divider-horizontal"></div>
-            <NumberInput className="pl-2 w-full text-3xl font-bold bg-transparent outline-none sm:pl-0 input" />
+            <NumberInput
+              className="pl-2 w-full text-3xl font-bold bg-transparent outline-none sm:pl-0 input"
+              value={value}
+              onChange={(e) => onChange(e.target.value)}
+            />
           </div>
         </div>
       </>
