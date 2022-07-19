b1a3cddcd8eb17d85b6e0899fc6fd6810b1b2d5c
inaridiy <inainainariinainari@gmail.com>
2022-07-19 12:06:08 +0900

    Completed fetchPairs

--- projects/interface/src/features/Swap/api/fetchPairs.ts
+++ projects/interface/src/features/Swap/api/fetchPairs.ts
@@ -2,17 +2,17 @@ import { chains } from "@/constant/chains";
 import { MULTI_CALL } from "@/constant/multicall";
 import { UniswapV2Pair__factory } from "@/libs/contracts";
 import { Multicall__factory } from "@/libs/contracts/factories";
-import { Pair, Token } from "@penta-swap/sdk";
-import { ethers } from "ethers";
+import { Pair, Token, TokenAmount } from "@penta-swap/sdk";
+import { BigNumber, ethers } from "ethers";
 
 export const fetchTokenPairs = async (
-  pairs: [Token, Token][],
+  pairTokens: [Token, Token][],
   chainName: chains,
   provider: ethers.providers.Provider
 ) => {
   const pairInterface = UniswapV2Pair__factory.createInterface();
   const multiCall = Multicall__factory.connect(MULTI_CALL[chainName], provider);
-  const pairAddresses = pairs.map(([tokenA, tokenB]) =>
+  const pairAddresses = pairTokens.map(([tokenA, tokenB]) =>
     Pair.getAddress(tokenA, tokenB)
   );
   const callData = pairInterface.encodeFunctionData("getReserves");
@@ -23,6 +23,27 @@ export const fetchTokenPairs = async (
   }));
 
   const [blockNumber, results] = await multiCall.callStatic.aggregate(calls);
-  console.log(blockNumber);
-  console.log(results);
+  const pairs = results
+    .map((result, i) => {
+      if (result === "0x") {
+        return;
+      } else {
+        const [tokenA, tokenB] = pairTokens[i];
+        const [reserves0, reserves1] = pairInterface.decodeFunctionResult(
+          "getReserves",
+          result
+        ) as [BigNumber, BigNumber];
+        const balances = (
+          tokenA.sortsBefore(tokenB)
+            ? [reserves0, reserves1]
+            : [reserves1, reserves0]
+        ) as [BigNumber, BigNumber];
+        return new Pair(
+          new TokenAmount(tokenA, balances[0].toString()),
+          new TokenAmount(tokenB, balances[1].toString())
+        );
+      }
+    })
+    .filter((pair) => Boolean(pair));
+  return [blockNumber, pairs];
 };
